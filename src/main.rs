24dcd04f735931a7a9c89f975449b03719caf29a
Fall1ngStar <12586447+Fall1ngStar@users.noreply.github.com>
2024-04-30 11:59:51 -0400

    Call the API inside the function body

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env::set_var;
+use std::env::{self, set_var};
 
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 
@@ -12,7 +12,19 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
         .query_string_parameters_ref()
         .and_then(|params| params.first("name"))
         .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+
+    // curl -v -H 'Content-Type: application/json' -H "api-key: $NEW_RELIC_LICENSE_KEY" -X POST https://otlp.nr-data.net/v1/traces
+    let client = reqwest::Client::new();
+
+    let response = client
+        .post(env::var("NEW_RELIC_OPENTELEMETRY_ENDPOINT")?)
+        .header("Content-Type", "application/json")
+        .header("api-key", env::var("NEW_RELIC_LICENSE_KEY")?)
+        .send()
+        .await?;
+    let bytes = response.bytes().await?.to_vec();
+    let message = String::from_utf8(bytes)?;
+    // let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
