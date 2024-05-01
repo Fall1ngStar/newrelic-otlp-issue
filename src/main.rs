bd22d56fa3b8ca02da084bbaae30f4ea59f9ad0b
Keegan Mullaney <kmullaney@newrelic.com>
2024-04-30 19:18:36 -0700

    add exporter retries

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env::{self, set_var};
+use std::env::{set_var};
 
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 
@@ -14,17 +14,17 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
         .unwrap_or("world");
 
     // curl -v -H 'Content-Type: application/json' -H "api-key: $NEW_RELIC_LICENSE_KEY" -X POST https://otlp.nr-data.net/v1/traces
-    let client = reqwest::Client::new();
+    // let client = reqwest::Client::new();
 
-    let response = client
-        .post(env::var("NEW_RELIC_OPENTELEMETRY_ENDPOINT")?)
-        .header("Content-Type", "application/json")
-        .header("api-key", env::var("NEW_RELIC_LICENSE_KEY")?)
-        .send()
-        .await?;
-    let bytes = response.bytes().await?.to_vec();
-    let message = String::from_utf8(bytes)?;
-    // let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+    // let response = client
+    //     .post(env::var("NEW_RELIC_OPENTELEMETRY_ENDPOINT")?)
+    //     .header("Content-Type", "application/json")
+    //     .header("api-key", env::var("NEW_RELIC_LICENSE_KEY")?)
+    //     .send()
+    //     .await?;
+    // let bytes = response.bytes().await?.to_vec();
+    // let message = String::from_utf8(bytes)?;
+    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
